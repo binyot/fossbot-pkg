0003090fa2e61b593d63049e4275cfb056ffac7a
binyot <sxterminal@protonmail.com>
2020-03-07 23:09:08 +0300

    added reset-usb script

---
+++ reset-usb.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo Resetting all usb devices
+
+for i in /sys/bus/pci/drivers/[uoex]hci_hcd/*:*; do
+  [ -e "$i" ] || continue
+  echo "${i##*/}" > "${i%/*}/unbind"
+  echo "${i##*/}" > "${i%/*}/bind"
+done
